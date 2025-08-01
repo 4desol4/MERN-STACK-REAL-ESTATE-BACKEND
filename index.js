43a429c205a1eb05b5545579b915a5b78fb86677
4desol4 <adeleyeshola0@gmail.com>
2025-08-01 20:30:39 +0100

    Initial commit - MERN stack app

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { userRoute } from './routes/userRoute.js';
+import { residencyRoute } from './routes/residencyRoute.js';
+dotenv.config()
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}))
+
+app.get('/api/test', (req, res) => {
+    console.log('TEST ENDPOINT HIT!');
+    res.json({ message: 'Server is working!' });
+});
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.use('/api/user', userRoute)
+app.use("/api/residency", residencyRoute)
